bd9b05ca8dbb49bed3ef2fc65dca8e2d80742533
tangzhiling <zhiling.tang@kaust.edu.sa>
2021-02-21 19:37:58 +0300

    Week 7 Homework leetcode 146 LRU cache

---
+++ Week_08/leetcode_146_LRU_cache.py
@@ -0,0 +1,39 @@
+"""
+Design a data structure that follows the constraints of a Least 
+Recently Used (LRU) cache.
+
+Implement the LRUCache class:
+
+LRUCache(int capacity) Initialize the LRU cache with positive size 
+capacity.
+
+int get(int key) Return the value of the key if the key exists, 
+otherwise return -1.
+
+void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+
+"""
+
+from collections import OrderedDict
+class LRUCache(OrderedDict):
+
+    def __init__(self, capacity):
+        self.dic = collections.OrderedDict()
+        self.remain = capacity
+
+    def get(self, key):
+        if key not in self.dic:
+            return -1
+        v = self.dic.pop(key) 
+        self.dic[key] = v   # set key as the newest one
+        return v
+
+    def put(self, key, value):
+        if key in self.dic:    
+            self.dic.pop(key)
+        else:
+            if self.remain > 0:
+                self.remain -= 1  
+            else:  # self.dic is full
+                self.dic.popitem(last=False) 
+        self.dic[key] = value
